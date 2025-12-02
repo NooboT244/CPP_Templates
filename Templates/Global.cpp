4815390ec6cf606052f90429f579174f396aafa6
MD. Rashidul Hasan <hamimhasan244@gmail.com>
2025-12-02 13:16:00 +0600

    Add files via upload

---
+++ Templates/Global.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+const void lightningSpeed()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+#define printArrays(arr)   \
+    for (auto &c : arr)    \
+    {                      \
+        cout << c << "\n"; \
+    }
+
+const double PI = 3.141592653589793;
+
+void solve();
+void solveT()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
+
+int32_t main()
+{
+    lightningSpeed();
+    solveT();
+    // solve();
+    return 0;
+}
+
+void solve()
+{
+
+}
