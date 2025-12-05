652db7ce09ce8fccfdfb9e0174024461074e9b3f
MD. Rashidul Hasan <hamimhasan244@gmail.com>
2025-12-05 23:57:23 +0600

    Update STLS.cpp

--- Templates/STLS.cpp
+++ Templates/STLS.cpp
@@ -1,6 +1,6 @@
-vector<any-datatype> var_name;         //dynamic array can increase size
-vector<any-datatype> var_name(n);      //n size dynamic array                       //TC: O(1)
-vector<any-datatype> var_name(n,x);    //n size x initialized dynamic array
+vector<any-datatype> var_name;                 //dynamic array can increase size
+vector<any-datatype> var_name(n);              //n size dynamic array                       TC: O(1)
+vector<any-datatype> var_name(n,x);            //n size x initialized dynamic array
 
 
 set<any-datatype> var_name;                    //a dynamic array but district value and sorted     TC: O(logn)
@@ -9,4 +9,13 @@ unordered_set<any-datatype> var_name;          //a dynamic array but district va
 unordered_multiset<any-datatype> var_name;     //a dynamic array                                   TC: O(1)
 
 
-pair<any-datatype,any-datadype> var_name;
+pair<any-datatype,any-datadype> var_name;      // store two values or pair wise value;             TC: O(1)
+
+
+
+map<any-key, any-value> var_name;                      // sorted + unique keys                  TC: O(logn)
+multimap<any-key, any-value> var_name2;                // sorted + duplicate keys               TC: O(logn)
+unordered_map<any-key, any-value> var_name;            // hashing + unique keys                 TC: O(1)
+unordered_multimap<any-key, any-value> var_name2;      // hashing + duplicate keys              TC: O(1)
+
+
