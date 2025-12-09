e4bcb14d3d96b5954bba32870ab0b18df3c7893a
MD. Rashidul Hasan <hamimhasan244@gmail.com>
2025-12-09 22:04:53 +0600

    Update Sorting.cpp

--- Templates/Sorting.cpp
+++ Templates/Sorting.cpp
@@ -1,1 +1,31 @@
+/*this is a selection sort algorithm*/
+void selection_sort(vector<int> &v, char flag)
+{
+    if (flag != 'g' && flag != 's')
+    {
+        throw invalid_argument("flag must be 'g' or 's'");
+    }
 
+    for (int i = 0; i < v.size(); i++)
+    {
+        int sii = i;
+        for (int j = (i + 1); j < v.size(); j++)
+        {
+            if (flag == 's' && v[sii] > v[j])
+            {
+                sii = j;
+            }
+            else if (flag == 'g' && v[sii] < v[j])
+            {
+                sii = j;
+            }
+        }
+
+        if (sii != i)
+        {
+            v[i] += v[sii];
+            v[sii] = v[i] - v[sii];
+            v[i] -= v[sii];
+        }
+    }
+}
